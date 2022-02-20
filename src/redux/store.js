d960e3a62fa72f55a70236197c528c7c5bd83c07
YanaSheri <yana.shereheda@gmail.com>
2022-02-21 00:27:22 +0200

    tools partially

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,69 @@
-import { createStore, combineReducers } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+// import { createStore, combineReducers } from "redux";
+// import { devToolsEnhancer } from "redux-devtools-extension";
 import contactReduce from "./reducer";
 
-const rootReduce = combineReducers({
-  contacts: contactReduce,
+import { configureStore } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "items",
+  storage,
+  whitelist: ["items"],
+};
+
+const logger = createLogger({
+  collapsed: (getState, action, logEntry) =>
+    !logEntry.error,
+  timestamp: false,
 });
 
-const store = createStore(
-  rootReduce,
-  devToolsEnhancer()
-);
+const store = configureStore({
+  reducer: {
+    contacts: persistReducer(
+      persistConfig,
+      contactReduce
+    ),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER,
+        ],
+      },
+    }).concat(logger),
+  devTools: process.env.NODE_ENV !== "production",
+});
 
-export default store;
+const persistor = persistStore(store);
+
+
+// const rootReduce = combineReducers({
+//   contacts: contactReduce,
+// });
+
+// const store = createStore(
+//   rootReduce,
+//   devToolsEnhancer()
+// );
+
+// export default store;
+
+
+export { store, persistor };
